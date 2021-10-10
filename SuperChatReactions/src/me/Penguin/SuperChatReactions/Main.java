dc483f0f8e597d931d0dc7802e64e9e0c16dda09
MLGPenguin <mlgpenguboi@gmail.com>
2021-10-10 14:25:34 +1000

    + Word Scrambles

--- SuperChatReactions/src/me/Penguin/SuperChatReactions/Main.java
+++ SuperChatReactions/src/me/Penguin/SuperChatReactions/Main.java
@@ -1,8 +1,14 @@
 package me.Penguin.SuperChatReactions;
 
 
-import org.bukkit.plugin.java.JavaPlugin;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import me.Penguin.SuperChatReactions.util.config;
 import me.Penguin.SuperChatReactions.util.m;
 
 public class Main extends JavaPlugin {
@@ -14,10 +20,45 @@ public class Main extends JavaPlugin {
 		new MainListener(this);
 		new MainCmd(this);		
 		
-		//getConfig().options().copyDefaults();
-		//saveDefaultConfig();
+		getConfig().options().copyDefaults();
+		saveDefaultConfig();
+		
+		config.setup();	
+		
+		new BukkitRunnable() {
+			
+			@Override
+			public void run() {
+				String word = chooseWord();
+				if (shouldUnscramble()) word = scramble(word);
+			}
+		}.runTaskTimer(this, config.reactionInterval, config.reactionInterval);
+		
 		
 	}
+	
+	private boolean shouldUnscramble() {
+		Random r = new Random();
+		int chance = config.unscrambleChance;
+		return (r.nextInt(101) <= chance);
+	}
+	
+	private String scramble(String word) {
+		List<String> letters = new ArrayList<>();
+		for (String x : word.split("")) letters.add(x);
+		String scrambled = "";
+		while (letters.size() > 0) {
+			int val = new Random().nextInt(letters.size());
+			scrambled = scrambled + letters.get(val);
+			letters.remove(val);
+		}
+		return scrambled;
+	}
+	
+	private String chooseWord() {
+		return config.words.get(new Random().nextInt(config.words.size()));
+	}
+	
 	
 
 }
